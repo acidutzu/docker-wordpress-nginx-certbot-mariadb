5e366f478c414dad874b00de8f98c022f1cf41cc
ADR-raspberry <ludusnetfree@gmail.com>
2022-06-27 10:15:03 +0300

    forth

--- runFirst-certbot-tls.sh
+++ runFirst-certbot-tls.sh
@@ -15,14 +15,14 @@ docker-compose up -d
 # Create a dummy TLS certificate and private key and restart NGINX to load the dummy certificate
 dirname=$(echo $(basename $(pwd)) | sed "s/\.//")
 path=/var/lib/docker/volumes/${dirname}_certbot/_data/live/$domain/
-mkdir -p $path && openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout ${path}privkey.pem -out ${path}fullchain.pem -subj '/CN=localhost'
+mkdir -p $path && openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout ${path}privkey.pem -out ${path}fullchain.pem -subj '/CN=localhost'
 docker-compose restart nginx
 
 ########## Remove the dummy certificate and private key before requesting a signed TLS certificate with Certbot
-#rm -r $(dirname $path)
+rm -r $(dirname $path)
 
 ######### Request a signed TLS certificate with Certbot and restart NGINX to load the new certificate
-#docker-compose run --rm --entrypoint "certbot certonly --webroot --webroot-path=/var/www/html --email $email --agree-tos --eff-email --force-renewal -d $domain -d www.$domain" certbot
-#docker-compose restart nginx
+docker-compose run --rm --entrypoint "certbot certonly --webroot --webroot-path=/var/www/html --email $email --agree-tos --eff-email --force-renewal -d $domain" certbot
+docker-compose restart nginx
 
 # systemctl enable docker
